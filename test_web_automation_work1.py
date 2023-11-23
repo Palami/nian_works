67f32206e11ede80ca822fcc407ca6f8c79e3ce7
lrrong <729924009@qq.com>
2023-11-23 19:18:36 +0800

    web自动化作业L1

--- test_web_automation_work1.py
+++ test_web_automation_work1.py
@@ -3,29 +3,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.parametrize("value,expectation",
-                         [("Selenium", ""),
-                          ("Appium", ""),
-                          ("面试", "")])
-def test_work1(value, expectation):
-    driver = webdriver.Chrome()
-    # 打开论坛
-    driver.get("https://ceshiren.com/")
-    # 隐式等待
-    driver.implicitly_wait(4)
-    # 定位高级搜索按钮
-    driver.find_element(By.ID, "search-button")
-    # 点击跳转高级搜索页面
-    driver.find_element(By.LINK_TEXT, "/search?expanded=true")
-    # 定位高级页面的搜索框
-    search_edit = driver.find_element(By.CSS_SELECTOR, "")
-    # 输入搜索关键字
-    search_edit.send_keys(value)
-    # 定位搜索按钮
-    search_button = driver.find_element(By.CSS_SELECTOR, "")
-    # 点击搜索按钮
-    search_button.click()
-    # 定位第一条搜索结果的标题
-    pass
-    # 断言
-    assert
+class Test_demo:
+
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+
+    # 后置处理：收尾
+    def teardown(self):
+        self.driver.quit()
+
+    @pytest.mark.parametrize("value",["Selenium","Appium","面试"])
+    def test_work1(self, value):
+        # 打开论坛
+        self.driver.get("https://ceshiren.com/")
+        # 隐式等待
+        self.driver.implicitly_wait(4)
+        # 定位高级搜索按钮并点击
+        self.driver.find_element(By.ID, "search-button").click()
+        # 点击跳转高级搜索页面
+        self.driver.find_element(By.CSS_SELECTOR, "[title='打开高级搜索']").click()
+        # 定位高级页面的搜索框
+        search_edit = self.driver.find_element(By.CSS_SELECTOR, "[placeholder='搜索']")
+        # 输入搜索关键字
+        search_edit.send_keys(value)
+        # 定位搜索按钮
+        search_button = self.driver.find_element(By.CSS_SELECTOR, ".search-cta")
+        # 点击搜索按钮
+        search_button.click()
+        # 定位第一条搜索结果的标题
+        text = self.driver.find_element(By.CSS_SELECTOR,".topic-title").text
+        # 断言
+        assert value.lower() in text.lower()
